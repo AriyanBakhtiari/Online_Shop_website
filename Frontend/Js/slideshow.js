c96f8cfa506f7fea6c6bfbc592ba1531f6328bbd
Ariyan Bakhtiari <ariyanbakhtiari@gmail.com>
2023-02-18 19:55:44 +0330

    change navbar dropdown

--- Frontend/Js/slideshow.js
+++ Frontend/Js/slideshow.js
@@ -44,6 +44,6 @@ function showSlides(n) {
         }
         slides[slideIndex - 1].style.display = "block";
         dots[slideIndex - 1].className += " active";
-        setTimeout(showSlides, 10000); // Change image every 2 seconds
+        setTimeout(showSlides, 5000); // Change image every 2 seconds
     }
 }
