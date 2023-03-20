de5d3d56e7a7be5af60b27c99daff27f888c76f8
Ariyan Bakhtiari <ariyanbakhtiari@gmail.com>
2023-03-20 20:26:30 +0330

    [Add] add remove product from order list , add order history

--- Frontend/Js/login-form.js
+++ Frontend/Js/login-form.js
@@ -16,7 +16,7 @@ function showLoginForm() {
 window.addEventListener("load", () => {
     const form = document.getElementsByTagName("form");
 
-    form[0].addEventListener("submit", function () {
+    form[0].addEventListener("submit", function (event) {
         event.preventDefault();
 
         var validation = loginValidation();
@@ -26,7 +26,7 @@ window.addEventListener("load", () => {
         loginrequest(this.elements);
     });
 
-    form[1].addEventListener("submit", function () {
+    form[1].addEventListener("submit", function (event) {
         event.preventDefault();
         var validation = signUpValidation();
         if (!validation)
--- Frontend/Js/userInfo.js
+++ Frontend/Js/userInfo.js
@@ -2,6 +2,7 @@ import { getRequest, postRequest } from './apicall.js';
 
 window.onload = getUserInfo();
 window.onload = getUserOrderList();
+window.onload = getOrderHistory();
 
 async function getUserInfo() {
     const res = await getRequest("User");
@@ -51,7 +52,21 @@ async function analyseUserInfo(res) {
 
 async function getUserOrderList() {
     const res = await getRequest("Order");
-    analyseUserOrderList(res);
+    await analyseUserOrderList(res);
+
+    document.getElementById("finalize-purchase").addEventListener("click", function () {
+        event.preventDefault();
+        finalizePurchase();
+    });
+
+
+    const removeButton = document.querySelectorAll(".remove-product")
+    removeButton.forEach(button => {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            removeProduct(event);
+        });
+    })
 }
 async function analyseUserOrderList(res) {
     if (res.status == 200) {
@@ -85,18 +100,30 @@ async function analyseUserOrderList(res) {
             var td5 = document.createElement("td");
             td5.innerText = orderDetail.price;
 
+            var td6 = document.createElement("td");
+            var button = document.createElement("button");
+            button.innerText = "حذف";
+            button.setAttribute("href", "#");
+            button.classList = "btn btn-light remove-product";
+            button.id = orderDetail.id;
+
+            td6.appendChild(button);
+
             tr.appendChild(td1);
             tr.appendChild(td2);
             tr.appendChild(td3);
             tr.appendChild(td4);
             tr.appendChild(td5);
+            tr.appendChild(td6);
 
             tbody.appendChild(tr);
         }
         orderListComponent = orderListComponent
             .replace("##body##", tbody.innerHTML)
             .replace("##TotalPrice##", res.data.totalPrice)
+            .replace("##id##", res.data.id)
 
+        orderListElement.innerHTML = "";
         orderListElement.insertAdjacentHTML("afterbegin", orderListComponent);
     }
     else {
@@ -107,7 +134,6 @@ document.getElementById("edit-user-form").addEventListener("submit", function ()
     event.preventDefault();
     editUserInfoPostRequst(this.elements);
 });
-
 async function editUserInfoPostRequst(formElements) {
     if (formElements.inputBirthdateYear.value == "" || formElements.inputBirthdateMonth.value == "" || formElements.inputBirthdateDay.value == "") {
         var birthDate = "";
@@ -133,6 +159,86 @@ async function editUserInfoPostRequst(formElements) {
     }
 }
 
+async function finalizePurchase() {
+
+
+    const id = document.getElementsByTagName("table")[0].id;
+    const res = await postRequest("Order/FinalizePurchase", {
+        orderId: id
+    });
+    if (res.status == 200) {
+        alert("درخواست با موفقیت انجام شد.");
+        location.reload();
+    }
+    else {
+        alert(res.data.errorMessage);
+    }
+}
+
+async function removeProduct(event) {
+    const res = await postRequest("Order/RemoveProduct", {
+        orderDetailId: event.target.id,
+    });
+    if (res.status == 200) {
+        getUserOrderList();
+    }
+    else {
+        alert(res.data.errorMessage);
+    }
+}
+
+async function getOrderHistory() {
+    const res = await getRequest("Order/OrderHistory");
+    await analyseOrderHistory(res);
+}
+async function analyseOrderHistory(res) {
+    if (res.status == 200) {
+        let orderHistory = await (await fetch("ComponentView/OrderHistory.html")).text();
+        let orderHistoryProduct = await (await fetch("ComponentView/OrderHistoryProduct.html")).text();
+        let historyBody = "";
+        for (let i = 0; i < res.data.orderList.length; i++) {
+            const orderList = res.data.orderList[i];
+            let order = orderHistory;
+            let product = "";
+
+            for (let i = 0; i < orderList.orderDetailList.length; i++) {
+                const orderDetailList = orderList.orderDetailList[i];
+                let orderPtoduct = orderHistoryProduct;
+                const name = orderDetailList.productName.length > 20 ? orderDetailList.productName.substring(0, 25) + "..." : orderDetailList.productName;
+                product += orderPtoduct.replace("##Name##", name)
+                    .replace("##Image##", orderDetailList.productImage)
+                    .replace("##Price##", orderDetailList.productPrice)
+            }
+
+            historyBody += order.replace("##TotalPrice##", orderList.totalPrice)
+                .replace("##OrderNum##", orderList.id)
+                .replace("##OrderDate##", orderList.finalizeDate)
+                .replace("##Product##", product);
+        }
+        document.getElementById("order-history").insertAdjacentHTML("afterbegin", historyBody);
+
+        var elms = document.getElementsByClassName('splide');
+        for (var i = 0; i < elms.length; i++) {
+            new Splide(elms[i], {
+
+                direction: 'rtl',
+                perPage: 5,
+                focus: 'center',
+                rewind: true,
+                speed: 400,
+                perMove: 1,
+                pagination: false,
+                drag: true,
+                autoWidth: true,
+                trimSpace: 'move',
+            }).mount();
+        }
+        $('#list-group a:nth-child(3)').tab('show')
+    }
+    else {
+        alert(res.data.errorMessage)
+    }
+}
 const sideButtonElement = document.querySelectorAll(".side-button");
 sideButtonElement.forEach(button => {
     button.addEventListener("click", function (event) {
