48d57c0e1cd9d9575475e24f5625793c9530428d
Ariyan Bakhtiari <ariyanbakhtiari@gmail.com>
2023-03-22 15:53:36 +0330

    [Change] change currency api call ,

--- Frontend/Js/index.js
+++ Frontend/Js/index.js
@@ -1,10 +1,14 @@
 import { getRequest, postRequest } from "./apiCall.js";
 
-// window.onload = CurrencyApiCall();
-// window.onload = CryptoCurrencyApiCall();
-// window.onload = GoldCurrencyApiCall();
+window.onload = CurrencyApiCall();
+window.onload = CryptoCurrencyApiCall();
 window.onload = getProductList();
 
+function toPersianDate(date) {
+    const datetime = new Date(date);
+    const persianDate = datetime.toLocaleDateString('fa-IR');
+    return persianDate;
+}
 
 async function getProductList() {
     const res = await getRequest('Products');
@@ -43,7 +47,7 @@ async function createProductCart(res) {
 }
 
 async function CurrencyApiCall() {
-    const res = await postRequest('CurrencyInquiry');
+    const res = await getRequest('CurrencyInquiry');
     createCurrencySummaryTable(res);
     createCurrencyTable(res);
 }
@@ -70,19 +74,19 @@ function createCurrencySummaryTable(res) {
 
         const td1 = document.createElement("td");
         const image = document.createElement("img");
-        image.setAttribute("src", res.CurrencyInquiryDataList[i].Icon);
+        image.setAttribute("src", res.data[i].icon);
         image.className = "avatar-currency";
         td1.appendChild(image);
 
         const name = document.createElement("span");
-        name.textContent = res.CurrencyInquiryDataList[i].ShowName;
+        name.textContent = res.data[i].showName;
         td1.appendChild(name);
         row.appendChild(td1);
 
 
         const price = document.createElement("td");
         price.className = "persian-num text-center";
-        price.textContent = toFarsiNumber(res.CurrencyInquiryDataList[i].Price);
+        price.textContent = res.data[i].price;
         row.appendChild(price);
 
         tblBody.appendChild(row);
@@ -145,39 +149,39 @@ function createCurrencyTable(res) {
 
     tblHead.appendChild(rowHeader)
 
-    for (let i = 0; i < res.CurrencyInquiryDataList.length; i++) {
+    for (let i = 0; i < res.data.length; i++) {
         if (i % 2 == 0) {
             const row = document.createElement("tr");
 
             const td1 = document.createElement("td");
             const image = document.createElement("img");
-            image.setAttribute("src", res.CurrencyInquiryDataList[i].Icon);
+            image.setAttribute("src", res.data[i].icon);
             image.className = "avatar-currency";
             td1.appendChild(image);
 
             const name = document.createElement("span");
-            name.textContent = res.CurrencyInquiryDataList[i].ShowName;
+            name.textContent = res.data[i].showName;
             td1.appendChild(name);
             row.appendChild(td1);
 
             const price = document.createElement("td");
-            price.textContent = toFarsiNumber(res.CurrencyInquiryDataList[i].Price);
+            price.textContent = res.data[i].price;
             row.appendChild(price);
 
             const changePrice = document.createElement("td");
-            changePrice.textContent = res.CurrencyInquiryDataList[i].Change24H;
+            changePrice.textContent = res.data[i].change24H;
             row.appendChild(changePrice);
 
             const lowestPrice = document.createElement("td");
-            lowestPrice.textContent = res.CurrencyInquiryDataList[i].LowestPrice;
+            lowestPrice.textContent = res.data[i].lowestPrice;
             row.appendChild(lowestPrice);
 
             const highestPrice = document.createElement("td");
-            highestPrice.textContent = res.CurrencyInquiryDataList[i].HighestPrice;
+            highestPrice.textContent = res.data[i].highestPrice;
             row.appendChild(highestPrice);
 
             const lastUpdate = document.createElement("td");
-            lastUpdate.textContent = toPersianDate(res.CurrencyInquiryDataList[i].LastUpdate);
+            lastUpdate.textContent = toPersianDate(res.data[i].lastUpdate);
             row.appendChild(lastUpdate);
 
             tblBody.appendChild(row);
@@ -187,33 +191,33 @@ function createCurrencyTable(res) {
 
             const td1_2 = document.createElement("td");
             const image2 = document.createElement("img");
-            image2.setAttribute("src", res.CurrencyInquiryDataList[i].Icon);
+            image2.setAttribute("src", res.data[i].icon);
             image2.className = "avatar-currency";
             td1_2.appendChild(image2);
 
             const name2 = document.createElement("span");
-            name2.textContent = res.CurrencyInquiryDataList[i].ShowName;
+            name2.textContent = res.data[i].showName;
             td1_2.appendChild(name2);
             row2.appendChild(td1_2);
 
             const price2 = document.createElement("td");
-            price2.textContent = toFarsiNumber(res.CurrencyInquiryDataList[i].Price);
+            price2.textContent = res.data[i].price;
             row2.appendChild(price2);
 
             const changePrice2 = document.createElement("td");
-            changePrice2.textContent = res.CurrencyInquiryDataList[i].Change24H;
+            changePrice2.textContent = res.data[i].change24H;
             row2.appendChild(changePrice2);
 
             const lowestPrice2 = document.createElement("td");
-            lowestPrice2.textContent = res.CurrencyInquiryDataList[i].LowestPrice;
+            lowestPrice2.textContent = res.data[i].lowestPrice;
             row2.appendChild(lowestPrice2);
 
             const highestPrice2 = document.createElement("td");
-            highestPrice2.textContent = res.CurrencyInquiryDataList[i].HighestPrice;
+            highestPrice2.textContent = res.data[i].highestPrice;
             row2.appendChild(highestPrice2);
 
             const lastUpdate2 = document.createElement("td");
-            lastUpdate2.textContent = toPersianDate(res.CurrencyInquiryDataList[i].LastUpdate);
+            lastUpdate2.textContent = toPersianDate(res.data[i].lastUpdate);
             row2.appendChild(lastUpdate2);
 
             tblBody2.appendChild(row2);
@@ -235,7 +239,7 @@ function createCurrencyTable(res) {
 }
 
 async function CryptoCurrencyApiCall() {
-    const res = await postRequest('CryptoCurrencyInquiry');
+    const res = await getRequest('CryptoCurrencyInquiry');
     createcryptoCurrencySummaryTable(res);
     createCryptoCurrencyTable(res);
 }
@@ -262,19 +266,19 @@ function createcryptoCurrencySummaryTable(res) {
 
         const td1 = document.createElement("td");
         const image = document.createElement("img");
-        image.setAttribute("src", res.CryptoCurrencyInquiryDataList[i].Icon);
+        image.setAttribute("src", res.data[i].icon);
         image.className = "avatar-currency";
         td1.appendChild(image);
 
         const name = document.createElement("span");
-        name.textContent = res.CryptoCurrencyInquiryDataList[i].ShowName;
+        name.textContent = res.data[i].showName;
         td1.appendChild(name);
         row.appendChild(td1);
 
 
         const price = document.createElement("td");
         price.className = "persian-num text-center";
-        price.textContent = res.CryptoCurrencyInquiryDataList[i].Price;
+        price.textContent = res.data[i].price;
         row.appendChild(price);
 
         tblBody.appendChild(row);
@@ -330,32 +334,32 @@ function createCryptoCurrencyTable(res) {
 
     tblHead.appendChild(rowHeader)
 
-    for (let i = 0; i < res.CryptoCurrencyInquiryDataList.length; i++) {
+    for (let i = 0; i < res.data.length; i++) {
         if (i % 2 == 0) {
             const row = document.createElement("tr");
 
             const td1 = document.createElement("td");
             const image = document.createElement("img");
-            image.setAttribute("src", res.CryptoCurrencyInquiryDataList[i].Icon);
+            image.setAttribute("src", res.data[i].icon);
             image.className = "avatar-currency";
             td1.appendChild(image);
 
             const name = document.createElement("span");
-            name.textContent = res.CryptoCurrencyInquiryDataList[i].ShowName;
+            name.textContent = res.data[i].showName;
             td1.appendChild(name);
             row.appendChild(td1);
 
             const price = document.createElement("td");
-            price.textContent = toFarsiNumber(res.CryptoCurrencyInquiryDataList[i].Price);
+            price.textContent = res.data[i].price;
             row.appendChild(price);
 
             const changePrice = document.createElement("td");
-            changePrice.textContent = res.CryptoCurrencyInquiryDataList[i].Change24H;
+            changePrice.textContent = res.data[i].change24H;
             row.appendChild(changePrice);
 
 
             const lastUpdate = document.createElement("td");
-            lastUpdate.textContent = toPersianDate(res.CryptoCurrencyInquiryDataList[i].LastUpdate);
+            lastUpdate.textContent = toPersianDate(res.data[i].lastUpdate);
             row.appendChild(lastUpdate);
 
             tblBody.appendChild(row);
@@ -366,25 +370,25 @@ function createCryptoCurrencyTable(res) {
 
             const td1_2 = document.createElement("td");
             const image2 = document.createElement("img");
-            image2.setAttribute("src", res.CryptoCurrencyInquiryDataList[i].Icon);
+            image2.setAttribute("src", res.data[i].icon);
             image2.className = "avatar-currency";
             td1_2.appendChild(image2);
 
             const name2 = document.createElement("span");
-            name2.textContent = res.CryptoCurrencyInquiryDataList[i].ShowName;
+            name2.textContent = res.data[i].showName;
             td1_2.appendChild(name2);
             row2.appendChild(td1_2);
 
             const price2 = document.createElement("td");
-            price2.textContent = toFarsiNumber(res.CryptoCurrencyInquiryDataList[i].Price);
+            price2.textContent = res.data[i].price;
             row2.appendChild(price2);
 
             const changePrice2 = document.createElement("td");
-            changePrice2.textContent = res.CryptoCurrencyInquiryDataList[i].Change24H;
+            changePrice2.textContent = res.data[i].change24H;
             row2.appendChild(changePrice2);
 
             const lastUpdate2 = document.createElement("td");
-            lastUpdate2.textContent = toPersianDate(res.CryptoCurrencyInquiryDataList[i].LastUpdate);
+            lastUpdate2.textContent = toPersianDate(res.data[i].lastUpdate);
             row2.appendChild(lastUpdate2);
 
             tblBody2.appendChild(row2);
@@ -403,64 +407,4 @@ function createCryptoCurrencyTable(res) {
     element.appendChild(tbl);
     element.appendChild(tbl2);
 
-}
-
-async function GoldCurrencyApiCall() {
-    const res = await postRequest('GoldPriceInquiry');
-    createGoldCurrencySummaryTable(res);
-}
-function createGoldCurrencySummaryTable(res) {
-    const element = document.getElementById("gold-currency-list-group");
-    const tbl = document.createElement("table");
-    const tblBody = document.createElement("tbody");
-    const tblHead = document.createElement("thead");
-    const rowHeader = document.createElement("tr");
-
-    const th1 = document.createElement("th");
-    th1.textContent = "عنوان";
-    rowHeader.appendChild(th1);
-
-    const th2 = document.createElement("th");
-    th2.textContent = "قیمت";
-    th2.classList = "text-center";
-    rowHeader.appendChild(th2);
-
-    tblHead.appendChild(rowHeader)
-
-    for (let i = 0; i < 3; i++) {
-        const row = document.createElement("tr");
-
-        const td1 = document.createElement("td");
-        const name = document.createElement("span");
-        name.textContent = res.GoldPriceInquiryDataList[i].ShowName;
-        td1.appendChild(name);
-        row.appendChild(td1);
-
-
-        const price = document.createElement("td");
-        price.className = "persian-num text-center";
-        price.textContent = toFarsiNumber(res.GoldPriceInquiryDataList[i].Price);
-        row.appendChild(price);
-
-        tblBody.appendChild(row);
-    }
-    const detailRow = document.createElement("tr");
-    const td = document.createElement("td");
-    td.setAttribute("colspan", "2");
-    td.className = "text-center";
-    const detailRowButton = document.createElement("button");
-    detailRowButton.className = "btn btn-secondary btn-block";
-    detailRowButton.setAttribute("data-toggle", "modal");
-    detailRowButton.setAttribute("data-target", "#currencyModel");
-    detailRowButton.textContent = "جزییات بیشتر"
-    td.appendChild(detailRowButton);
-    detailRow.appendChild(td);
-    tblBody.appendChild(detailRow);
-
-    tbl.appendChild(tblHead);
-    tbl.appendChild(tblBody);
-    tbl.classList = "table table-sm";
-
-    element.innerHTML = "";
-    element.appendChild(tbl);
 }
